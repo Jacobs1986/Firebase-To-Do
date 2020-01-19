716553557e7d925bab65f86436c263c455359ae8
Sean Jacobson <sean.jacobson86@gmail.com>
2020-01-18 23:01:52 -0700

    Added and initialized the firebase database connection.

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCjxJr3u2T68TMUKk99v4Rr7wZbeiug0tA",
+    authDomain: "to-do-list-25176.firebaseapp.com",
+    databaseURL: "https://to-do-list-25176.firebaseio.com",
+    projectId: "to-do-list-25176",
+    storageBucket: "to-do-list-25176.appspot.com",
+    messagingSenderId: "1013406437671",
+    appId: "1:1013406437671:web:ed8ad1630f05a349129902"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
